a60c1918505cdcb84608d1e9fc702f48f56647c2
tq5124 <wutianqi124@gmail.com>
2014-03-14 12:01:26 +0800

    end url message display on console

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require("http");
+var url = require("url");
 
 http.createServer(function(request, response) {
+	var pathname = url.parse(request.url).pathname;
+	console.log("Request for " + pathname + " received.");
 	response.writeHead(200, {"Content-Type": "text/plain", "dyweb": "yes!"});
 	response.write("Hello World");
 	response.end();
